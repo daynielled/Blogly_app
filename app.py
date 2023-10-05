865ff60abfb9845d7988f36786b22d53154f6c4e
DD <ddonegal1@gmail.com>
2023-10-05 10:22:26 -0400

    still working on package versions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
@@ -20,5 +20,62 @@ db.create_all()
 
 @app.route('/')
 def index():
+    """Redirects to list of users"""
+    return redirect ("/users")
+
+@app.route('/users')
+def list_users():
+    """Show all users"""
     users = User.query.all()
-    return render_template('index.html', title ='Home', users=users )
+    return render_template('users_list.html', title= 'Users List', users=users)
+
+@app.route('/users/new', methods=['GET'])
+def new_user_form():
+    """Show the add user form"""
+    return render_template('add_user.html')
+
+@app.route('user/new', methods=['POST'])
+def add_user():
+    """Add new user"""
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    img_url = request.form['img_url', 'DEFAULT_PROFILE']
+
+    new_user = User(first_name=first_name, last_name=last_name, img_url=img_url)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>')
+def view_user(user_id):
+    """View a specific user"""
+    user = User.query.get_or_404(user_id)
+    return render_template('view_user.html', title='User Detail', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['GET', 'POST'])
+def edit_user(user_id):
+    """Edit user and return to user list"""
+    user = User.query.get_or_404(user_id)
+
+    if request.method == 'POST':
+        user.first_name = request.form.get('first_name')
+        user.last_name = request.form.get('last_name')
+        user.image_url = request.form.get('image_url', 'DEFAULT_PROFILE') 
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/users')
+    
+    return render_template('edit_user.html', title='Edit User', user=user)  
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    """Handle delete form submission"""
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
--- models.py
+++ models.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
+DEFAULT_PROFILE = "https://www.pngitem.com/pimgs/m/361-3619033_person-icon-one-person-icon-blue-hd-png.png"
+
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -13,6 +13,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
-    img_url = db.Column(db.Text, nullable=False, default= 'default_profile.png')
+    middle_name = db.Column(db.String)
+    img_url = db.Column(db.Text, nullable=False, default= 'DEFAULT_PROFILE')
 
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+def get_full_name(self):
+     if self.middle_name:
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
+     else:
+        return f"{self.first_name} {self.last_name}"
 
